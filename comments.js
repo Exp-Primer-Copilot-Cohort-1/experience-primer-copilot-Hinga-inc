a271b1fcd4dd908a2481753573c8e88f5dfc90f8
HINGA PETER <141874604+Hinga-inc@users.noreply.github.com>
2024-04-04 10:30:54 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,55 @@
 // create web server
-const http = require('http');
+// 1. create app
+// 2. create server
+// 3. listen to port 3000
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const { response } = require('express');
+const { json } = require('body-parser');
+const port = 3000;
+
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/comments', (req, res) => {
+    fs.readFile(path.join(__dirname, 'comments.json'), 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        res.send(data);
+    });
+});
+
+app.post('/comments', (req, res) => {
+    fs.readFile(path.join(__dirname, 'comments.json'), 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        const comments = JSON.parse(data);
+        comments.push({
+            id: comments.length + 1,
+            name: req.body.name,
+            comment: req.body.comment
+        });
+
+        fs.writeFile(path.join(__dirname, 'comments.json'), JSON.stringify(comments), (err) => {
+            if (err) {
+                throw err;
+            }
+            res.send('Comment added successfully');
+        });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
